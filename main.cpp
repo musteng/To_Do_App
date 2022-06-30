3a62732c32a578da9bc1c8915ab89e6318f81d2e
mustafacevik <2763>
2022-06-30 12:00:12 +0300

    First Commit.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "MainWindow.h"
+#include "QApplication"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    /**
+     * QWidget: Must construct a QApplication before a QWidget
+     * Before creating a main window object, QApplication must be created and initialized.
+     */
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    return a.exec();
+}
